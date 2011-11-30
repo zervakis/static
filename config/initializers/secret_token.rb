76c75fbeee1f9f26a5c2966ad073422f2753c682
Kostas Zervakis <zervakis@silicor.gr>
2011-11-30 21:51:00 +0200

    1st commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Static1::Application.config.secret_token = '7ccff5a477241901a9ed16fc9a1c1e5cda3a62ce1070c8dee19c193098264b748884415f70aed8e17d8f87f3f5f7fe09fa478626fc224ae11b8d2b4d9310ecbf'
